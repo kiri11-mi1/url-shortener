5a590e92822ea7e1ba257c7ff79948d7c3f198f0
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 15:11:12 +0700

    add short algo

--- internal/shorten/shorten.go
+++ internal/shorten/shorten.go
@@ -1,1 +1,26 @@
 package shorten
+
+const ALPHABET = "qwertyuiopasdfghjkzxcvbnmQWERTYUPASDFGHJKLZXCVBNM123456789"
+
+var ALPHABET_LEN = uint32(len(ALPHABET))
+
+func Shorten(id uint32) string {
+	var nums []uint32
+	var result string
+	for id > 0 {
+		nums = append(nums, id%ALPHABET_LEN)
+		id /= ALPHABET_LEN
+	}
+	for _, index := range getReverseArray(nums) {
+		result += string(ALPHABET[index])
+	}
+	return result
+}
+
+func getReverseArray(array []uint32) []uint32 {
+	var newArray []uint32
+	for i := len(array) - 1; i > -1; i-- {
+		newArray = append(newArray, array[i])
+	}
+	return newArray
+}
