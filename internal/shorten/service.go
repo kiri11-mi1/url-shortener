d87749308d56d9b68fc9f3c200c8227d7a43098e
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 17:55:20 +0700

    add interface for storage

--- internal/shorten/service.go
+++ internal/shorten/service.go
@@ -1,1 +1,20 @@
 package shorten
+
+import (
+	"context"
+	"url-shortener/internal/model"
+)
+
+type Storage interface {
+	Put(ctx context.Context, shortening model.Shortening) (*model.Shortening, error)
+	Get(ctx context.Context, identifier string) (*model.Shortening, error)
+	IncrementVisits(ctx context.Context, identifier string) error
+}
+
+type Service struct {
+	storage Storage
+}
+
+func NewService(storage Storage) *Service {
+	return &Service{storage: storage}
+}
