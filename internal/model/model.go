4f23ca4892f5e527cf1e02c85a9c4309dff9ae55
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 15:22:10 +0700

    add model shortening

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,19 @@
 package model
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound          = errors.New("not found identifier in database")
+	ErrIdentifierIsExist = errors.New("identifier already exist")
+)
+
+type Shortening struct {
+	Identifier  string    `json:"identifier"`
+	OriginalUrl string    `json:"original_url"`
+	Visits      string    `json:"visits"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
