3d13a64b9b6ad64a0d2a9a68b5f1f5e13ecb2d69
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 20:04:37 +0700

    add generating shortenings

--- internal/shorten/service.go
+++ internal/shorten/service.go
@@ -2,6 +2,7 @@ package shorten
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"url-shortener/internal/model"
 )
 
@@ -17,4 +18,21 @@ type Service struct {
 
 func NewService(storage Storage) *Service {
 	return &Service{storage: storage}
+}
+
+func (s *Service) Shorten(ctx context.Context, input model.ShortenInput) (*model.Shortening, error) {
+	var (
+		id         = uuid.New().ID()
+		identifier = input.Identifier.OrElse(Shorten(id))
+	)
+	dbShortening := model.Shortening{
+		Identifier:  identifier,
+		OriginalUrl: input.RawURL,
+	}
+
+	shortening, err := s.storage.Put(ctx, dbShortening)
+	if err != nil {
+		return nil, err
+	}
+	return shortening, nil
 }
