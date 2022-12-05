03871df8c16fb44137b065b6f476c5b6f89e0340
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 21:09:47 +0700

    test genarate shortener

--- internal/shorten/service_test.go
+++ internal/shorten/service_test.go
@@ -1,1 +1,27 @@
 package shorten_test
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"url-shortener/internal/model"
+	"url-shortener/internal/shorten"
+	"url-shortener/internal/storage/shortening"
+)
+
+func TestService_Shorten(t *testing.T) {
+	t.Run("generate shortening for a given url", func(t *testing.T) {
+		var (
+			svc   = shorten.NewService(shortening.NewInMemory())
+			input = model.ShortenInput{RawURL: "https://example.com"}
+		)
+		createdShortening, err := svc.Shorten(context.Background(), input)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, createdShortening.Identifier)
+		assert.Equal(t, input.RawURL, createdShortening.OriginalUrl)
+		assert.NotZero(t, createdShortening.CreatedAt)
+	})
+
+}
