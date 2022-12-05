529c5613b6b670688a2eda1f9b6b6f978aa685c9
Kirill Mil <kirill.mil.0310@mail.ru>
2022-12-05 15:35:15 +0700

    add func helper(return mongo collection) and mongo client

--- internal/storage/shortening/mongo.go
+++ internal/storage/shortening/mongo.go
@@ -1,1 +1,15 @@
 package shortening
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type mgo struct {
+	db *mongo.Database
+}
+
+func NewMongoDB(client *mongo.Client) *mgo {
+	return &mgo{db: client.Database("url-shortener")}
+}
+
+func (m *mgo) col() *mongo.Collection {
+	return m.db.Collection("shortenings")
+}
