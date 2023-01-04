df92d45529146239471910659c686eff9f235574
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-04 21:42:01 +0700

    add test for check new func

--- internal/shorten/shorten_test.go
+++ internal/shorten/shorten_test.go
@@ -2,6 +2,7 @@ package shorten_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"url-shortener/internal/shorten"
 )
@@ -30,5 +31,28 @@ func TestShorten(t *testing.T) {
 		for i := 0; i < 100; i++ {
 			assert.Equal(t, tc.expected, shorten.Shorten(tc.id))
 		}
+	})
+}
+
+func TestService_PrependBaseURL(t *testing.T) {
+	t.Run("get new url", func(t *testing.T) {
+		var (
+			baseURL    = "https://example.com"
+			identifier = "testId83"
+			expected   = "https://example.com/testId83"
+		)
+		var prependURL, err = shorten.PrependBaseURL(baseURL, identifier)
+		require.NoError(t, err)
+		assert.Equal(t, expected, prependURL)
+	})
+	t.Run("get new url", func(t *testing.T) {
+		var (
+			baseURL    = "https://example.com/kajjsndkajdasdna/oansdkjansjdk"
+			identifier = "testId83"
+			expected   = "https://example.com/testId83"
+		)
+		var prependURL, err = shorten.PrependBaseURL(baseURL, identifier)
+		require.NoError(t, err)
+		assert.Equal(t, expected, prependURL)
 	})
 }
