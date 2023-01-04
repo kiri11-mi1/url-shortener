f031d69bf8579c26e2eb35a2273e8e016b87d5d1
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-04 22:31:12 +0700

    add logic in config module

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,42 @@
 package config
 
+import (
+	"context"
+	"github.com/sethvargo/go-envconfig"
+	"log"
+	"strings"
+	"sync"
+)
+
 type Config struct {
 	BaseURL string `env:"BASE_URL, default=http://localhost:8080"`
+}
+
+var (
+	cfg  Config
+	once sync.Once
+)
+
+func Get() Config {
+	once.Do(func() {
+		lookuper := UpcaseLookuper(envconfig.OsLookuper())
+		if err := envconfig.ProcessWith(context.Background(), &cfg, lookuper); err != nil {
+			log.Fatal(err)
+		}
+	})
+	return cfg
+}
+
+type upcaseLookuper struct {
+	Next envconfig.Lookuper
+}
+
+func (l *upcaseLookuper) Lookup(key string) (string, bool) {
+	return l.Next.Lookup(strings.ToUpper(key))
+}
+
+func UpcaseLookuper(next envconfig.Lookuper) *upcaseLookuper {
+	return &upcaseLookuper{
+		Next: next,
+	}
 }
