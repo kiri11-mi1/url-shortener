d3b54a11e6aed326e508ca02880ea6b417e59ac5
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-10 00:58:49 +0700

    add health route

--- internal/server/handle_health.go
+++ internal/server/handle_health.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+func HandleHealth() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(
+			http.StatusOK,
+			HealthResponse{Status: "ok"},
+		)
+	}
+}
