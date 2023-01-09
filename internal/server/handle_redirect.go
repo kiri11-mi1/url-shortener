5b05017878d8c0efb242ffbdc174d9de62af6c14
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-10 02:51:10 +0700

    add redirect by original url

--- internal/server/handle_redirect.go
+++ internal/server/handle_redirect.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+	"log"
+	"net/http"
+	"url-shortener/internal/model"
+)
+
+type redirecter interface {
+	Redirect(ctx context.Context, identifier string) (string, error)
+}
+
+func HandleRedirect(redirecter redirecter) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		identifier := c.Param("identifier")
+		redirectURL, err := redirecter.Redirect(c.Request().Context(), identifier)
+		if err != nil {
+			if errors.Is(err, model.ErrNotFound) {
+				return echo.NewHTTPError(http.StatusNotFound, err.Error())
+			}
+			log.Printf("redirect failed: %v", err)
+			return echo.NewHTTPError(http.StatusInternalServerError)
+		}
+		return c.Redirect(http.StatusMovedPermanently, redirectURL)
+	}
+}
