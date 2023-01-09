69a905c17d4715a4a697fb26a2cec780f6fb96aa
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-09 16:53:14 +0700

    logic for validation requests

--- internal/server/validator.go
+++ internal/server/validator.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type requestValidator struct {
+	v *validator.Validate
+}
+
+func NewValidator() *requestValidator {
+	return &requestValidator{
+		v: validator.New(),
+	}
+}
+
+func (v *requestValidator) Validate(i any) error {
+	if err := v.v.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
