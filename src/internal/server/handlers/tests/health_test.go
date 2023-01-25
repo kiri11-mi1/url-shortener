425b6f4f4b0f8a84705236cf71d624aaffe2cffb
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-25 12:08:51 +0700

    add health test

--- src/internal/server/handlers/tests/health_test.go
+++ src/internal/server/handlers/tests/health_test.go
@@ -1,6 +1,10 @@
 package tests
 
 import (
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,7 +13,19 @@ import (
 
 func TestHandleHealth(t *testing.T) {
 	t.Run("check health service", func(t *testing.T) {
-		var expectedResponse = handlers.HealthResponse{Status: "ok"}
-		var request = httptest.NewRequest(http.MethodGet, "/api/health", nil)
+		var (
+			expectedResponse = handlers.HealthResponse{Status: "ok"}
+			request          = httptest.NewRequest(http.MethodGet, "/api/health", nil)
+			recorder         = httptest.NewRecorder()
+			handler          = handlers.HandleHealth()
+			e                = echo.New()
+			c                = e.NewContext(request, recorder)
+		)
+
+		var resultResp = new(handlers.HealthResponse)
+		require.NoError(t, handler(c))
+		require.NoError(t, json.Unmarshal(recorder.Body.Bytes(), resultResp))
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		assert.Equal(t, &expectedResponse, resultResp)
 	})
 }
